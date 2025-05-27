c51e02c59aa583e7d3da02709c310ec191f4422e
lea3738 <lea3738@users.noreply.github.com>
2025-05-27 11:00:33 +0200

    extension ready

--- chrome-extension/src/popup/Popup.tsx
+++ chrome-extension/src/popup/Popup.tsx
@@ -4,78 +4,84 @@ export default function Popup() {
   const [selectedCode, setSelectedCode] = useState<string>('');
   const [phase, setPhase] = useState<'select' | 'preview'>('select');
 
-  const handleSelectCode = async function() {
-    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-    const results = await chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        function: () => window.getSelection()?.toString() || ''
+  const handleSelectCode = async function () {
+    try {
+      const [tab] = await chrome.tabs.query({
+        active: true,
+        currentWindow: true,
       });
-    
-        const selection = results[0].result;
+      const results = await chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: () => window.getSelection()?.toString() || '',
+      });
 
-        if (selection && selection.trim()) {
-            setSelectedCode(selection);
-            setPhase('preview');
-        } else {
+      const selection = results[0].result;
+
+      if (selection && selection.trim()) {
+        setSelectedCode(selection);
+        setPhase('preview');
+      } else {
         alert('No code was selected, select that you want to add on the page');
-        }
-    }    
-
-    const handleCreateCard = function() {
-        if (selectedCode) {
-        const encodedCode = encodeURIComponent(selectedCode);
-        const frontendUrl = process.env.REACT_APP_FRONTEND_URL || 'http://localhost:3000';
-        const url = `${frontendUrl}/cards/new?code=${encodedCode}`;
-        chrome.tabs.create({ url });
-        window.close();
-        }
+      }
+    } catch (e) {
+      console.error('unable to get selection. Error: ', e);
     }
+  };
 
-    const handleBack = () => {
-        setPhase('select');
-        setSelectedCode('');
-    };
+  const handleCreateCard = function () {
+    if (selectedCode) {
+      const encodedCode = encodeURIComponent(selectedCode);
+      const frontendUrl =
+        process.env.REACT_APP_FRONTEND_URL || 'http://localhost:3000';
+      const url = `${frontendUrl}/cards/new?code=${encodedCode}`;
+      chrome.tabs.create({ url });
+      window.close();
+    }
+  };
 
-
+  const handleBack = () => {
+    setPhase('select');
+    setSelectedCode('');
+  };
 
   return (
-    <div>
-        {/* Select Phase */}
-        {phase === 'select' &&(
-            <div>
-              <p>Highlight your code</p>
-              <button
-                type="button"
-                className="mt-4 bg-blue-500 text-sm text-white px-4 py-1 w-full rounded hover:bg-blue-600"
-                onClick={handleSelectCode}
-              >
-                Add code
-              </button>
-            </div>
-          )}
-
-
-          {/* Preview Phase */}
-            {phase === 'preview' &&(
-                <div>
-                <p>Your code</p>
-                <p>{selectedCode}</p>
-                <button
-                    type="button"
-                    className="mt-4 bg-blue-500 text-sm text-white px-4 py-1 w-full rounded hover:bg-blue-600"
-                    onClick={handleCreateCard}
-                >
-                    Create study card
-                </button>
-                <button
-                    type="button"
-                    onClick={handleBack}
-                    className="bg-gray-500 hover:bg-gray-600 text-white px-4 py-2 rounded-md text-sm font-medium transition-colors"
-                >
-                    Go back
-              </button>
-                </div>
-            )}
+    <div className="flex flex-col justify-center m-4">
+      {/* Select Phase */}
+      {phase === 'select' && (
+        <div className="flex flex-col gap-4">
+          <p className="text-sm font-bold">Welcome to Study Cards</p>
+          <p className="text-sm">Highlight your code and click on Add code</p>
+          <button
+            type="button"
+            className="mt-4 bg-blue-500 text-sm text-white px-4 py-1 w-full rounded hover:bg-blue-600"
+            onClick={handleSelectCode}
+          >
+            Add code
+          </button>
         </div>
-    )
+      )}
+
+      {/* Preview Phase */}
+      {phase === 'preview' && (
+        <div className="flex flex-col gap-4">
+          <p className="font-bold text-sm">Your code:</p>
+          <p className="text-xs">{selectedCode}</p>
+          <button
+            type="button"
+            className="mt-4 bg-blue-500 text-sm text-white px-4 py-1 w-full rounded hover:bg-blue-600"
+            onClick={handleCreateCard}
+          >
+            Create a study card
+          </button>
+          <button
+            type="button"
+            onClick={handleBack}
+            className="bg-gray-500 hover:bg-gray-600 text-white px-4 py-1 w-full rounded text-sm"
+          >
+            Go back
+          </button>
+        </div>
+      )}
+    </div>
+  );
 }
