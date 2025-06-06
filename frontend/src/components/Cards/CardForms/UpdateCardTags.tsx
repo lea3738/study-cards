0386f5e4279810da3ee4d2d647cdf8dd90c9c2d1
lea3738 <lea3738@users.noreply.github.com>
2025-06-06 15:51:35 +0200

    petite correction pour enlever la max height dans le container des tags

--- frontend/src/components/Cards/CardForms/UpdateCardTags.tsx
+++ frontend/src/components/Cards/CardForms/UpdateCardTags.tsx
@@ -54,10 +54,10 @@ const handleAddTagName = useCallback((tagName: string) => {
   }, [updatedTagNames, allTagNames]);
 
   return (
-    <div className="w-full flex max-h-16 justify-between items-start border rounded-lg p-2 mb-2">
+    <div className="w-full flex justify-between items-start border rounded-lg p-2 mb-2">
       <div className="flex flex-col flex-1 min-w-0 mr-2">
         <p className="text-xs text-gray-600 flex-shrink-0">Tags</p>
-        <div className="gap-1 flex flex-wrap mt-1 overflow-y-auto max-h-14">
+        <div className="gap-1 flex flex-wrap mt-1">
           {updatedTagNames.map((tagName) => {
             return (
               <DeletableTag
